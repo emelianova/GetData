d99c44c110b80e78ca16a8aafc4318e547cecb76
emelianova <warnstorfia@mail.ru>
2015-01-26 14:48:51 +0600

    typo

--- run_analysis.R
+++ run_analysis.R
@@ -13,10 +13,9 @@ subject<- unlist(rbind(subject, read.table("UCI HAR Dataset/test/subject_test.tx
 activity<- read.table("UCI HAR Dataset/train/y_train.txt")
 activity<- rbind(activity, read.table("UCI HAR Dataset/test/y_test.txt"))                       
 
-varnames<- unlist(read.table("UCI HAR Dataset/features.txt",     # Step 2  
-                             colClasses="character")[,2])    
-colnums<- grep("-(mean|std)[(]", varnames)                     
-varnames<- varnames[colnums]
+names(data)<- unlist(read.table("UCI HAR Dataset/features.txt",     # Step 2  
+                                colClasses="character")[,2])    
+colnums<- grep("-(mean|std)[(]", names(data))                     
 data<- data[, colnums]                                       
 
 library(tidyr)                                                   # Step 3
@@ -24,10 +23,9 @@ activity<- spread(activity, V1, V1)
 colnames(activity)<- c("walking", "walkingup", "walkingdown","sitting", "staying", "laying")
 activity<- gather(activity, activity, value, na.rm=TRUE)[,1]   
 
-varnames<- gsub("[()]", "", varnames)                            # Step 4
-varnames<- gsub("BodyBody", "Body", varnames) 
-varnames<- gsub("[-]", ".", varnames)
-colnames(data)<- varnames
+names(data)<- gsub("[()]", "", names(data))                            # Step 4
+names(data)<- gsub("BodyBody", "Body", names(data)) 
+names(data)<- gsub("[-]", ".", names(data))
 data$subject<- subject
 data$activity<- activity
 
@@ -35,10 +33,10 @@ library(plyr)
 tidy<- ddply(data, .(activity, subject), numcolwise(mean))            # Step 5
 tidy<- gather(tidy, type, value, tBodyAcc.mean.X:fBodyGyroJerkMag.std)
 tidy<- separate(tidy, type, into=c("type", "estimator", "coordinate"), sep="[.]",
-                     extra="merge")
+                extra="merge")
 tidy$type<- tolower(gsub("([^k])([A-Z])", "\\1,\\2", tidy$type))
 tidy<- separate(tidy, type, into=c("domain", "source", "signal", "jerkmag"), 
-                    sep=",", extra="merge")
+                sep=",", extra="merge")
 tidy<- spread(tidy, estimator, value)
 
 write.table(tidy, file="final.txt", row.names=F)
